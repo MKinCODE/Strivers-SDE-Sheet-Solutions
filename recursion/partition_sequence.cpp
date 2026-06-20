475f2031ca216d437b1c808f10f4b8726656e04b
Mousam Kumawat <mousamkumawat1234@gmail.com>
2026-06-20 09:01:30 +0000

    other approaches added

--- recursion/partition_sequence.cpp
+++ recursion/partition_sequence.cpp
@@ -1,6 +1,63 @@
 #include<string>
 #include<algorithm>
+#include<vector>
 using namespace std;
+//most cleaner and optimal by removing computing fact again and again
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        vector<int> nums;
+        for(int i=1; i<=n; i++){
+            nums.push_back(i);
+        }
+        k--;
+        string ans="";
+        int fact=1;
+        for(int i=1; i<=n; i++){
+            fact*=i;
+        }
+        while(n>1){
+            fact=fact/n;
+            int index=k/fact;
+            ans+=nums[index]+'0';
+            nums.erase(nums.begin()+index);
+            k%=fact;
+            n--;
+        }
+        return ans+=(nums[0]+'0');
+    }
+};
+
+
+//more optimal
+class Solution {
+public:
+    int fact(int n){
+        int ans=1;
+        for(int i=1; i<=n; i++){
+            ans*=i;
+        }
+        return ans;
+    }
+    string getPermutation(int n, int k) {
+        vector<int> nums;
+        for(int i=1; i<=n; i++){
+            nums.push_back(i);
+        }
+        k--;
+        string ans="";
+        while(n>1){
+            int facto=fact(n-1);
+            int index=k/facto;
+            ans+=nums[index]+'0';
+            nums.erase(nums.begin()+index);
+            k%=facto;
+            n--;
+        }
+        return ans+=(nums[0]+'0');
+    }
+};
+
 
 //less optimal
 class Solution {
